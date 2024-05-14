eccaaba2d0de2500397a7822d69fad08e7714169
Grant <gsteere4717@xmail.dixietech.edu>
2024-05-14 11:06:08 -0600

    Add delete crud operations

--- crud.py
+++ crud.py
@@ -14,6 +14,14 @@ def get_user(db: Session, user_id: int):
 def get_users(db: Session, skip: int = 0, limit: int = 10):
     return db.query(schemas.User).offset(skip).limit(limit).all()
 
+def delete_user(db: Session, user_id: int):
+    user = db.query(schemas.User).filter(schemas.User.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return True
+    return False
+
 def create_recipe(db: Session, recipe: schemas.Recipes):
     db_recipe = schemas.Recipes(**schemas.Recipes.model_dump())
     db.add(db_recipe)
@@ -27,6 +35,13 @@ def get_recipe(db: Session, recipe_id: int):
 def get_recipes(db: Session, skip: int = 0, limit: int = 10):
     return db.query(schemas.Recipes).offset(skip).limit(limit).all()
 
+def delete_recipe(db: Session, recipe_id: int):
+    recipe = db.query(schemas.Recipes).filter(schemas.Recipes.id == recipe_id).first()
+    if recipe:
+        db.delete(recipe)
+        db.commit()
+        return True
+    return False
 
 def create_category(db: Session, category: schemas.Category):
     db_category = schemas.Category(**schemas.Category.model_dump())
